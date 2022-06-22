379674c52b2476e5b7d31a208d8c98d8efbbd824
Najwa983 <Najwa.lakmouri1@gmail.com>
2022-06-22 16:55:26 +0100

    connecting the client with the server and enabling CORS

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 const PORT = 8080;
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    }
+    });
+const STATIC_CHANNELS =['global_notifications', 'global_chat'];
 // console.log(io);
 
 http.listen(PORT, () => {
@@ -10,4 +16,5 @@ http.listen(PORT, () => {
 io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
 
     console.log('new client connected');
+    socket.emit('connection',null);
 })
