883628aad37ce4ae02afdedf0b8de89d2e152601
Najwa983 <Najwa.lakmouri1@gmail.com>
2022-06-25 12:19:46 +0100

    fetching the channels

--- client/src/chat/Chat.js
+++ client/src/chat/Chat.js
@@ -7,24 +7,32 @@ export class Chat extends React.Component {
     state = {
         channels: [{ id: 1, name: 'first', participants: 10 }]
     }
-    // componentDidMount() {
-    //     this.loadChannels();
-    // }
+    componentDidMount() {
+        this.loadChannels();
+    }
+
+    loadChannels = async () => {
+        fetch('http://localhost:8080/getChannels', ).then(async response => {
+                let data = await response.json();
+                this.setState({ channels: data.channels });
+            })
+    }
+    // The front end needs to handle these events as well. To capture the events we have to use the same event name that the back end emits and vice versa. When the client hits the channel, an event must be sent to the back end to calculate the number of participants and broadcast to all the current sockets that a new user entered the channel.
+    handleChannelSelect = id => {
+        this.socket.emit('channel-join', id, ack => {
+        });
+    }    
+
     
-    // loadChannels = async () => {    fetch('http://localhost:8080/getChannels').then(async response => {
-    //         let data = await response.json();
-    //             this.setState({ channels: data.channels });
-    //     })
-    // }
     render() {
         return (
-        
-                <div className="chat-app">‍
-                    <ChannelList channels={this.state.channels}></ChannelList>
-                    <MessagesPanel/>
-                </div>
 
-                
+            <div className="chat-app">‍
+                <ChannelList channels={this.state.channels} onselectchannel={this.handleChannelSelect}></ChannelList>
+                <MessagesPanel />
+            </div>
+
+
         );
     }
 }
--- server.js
+++ server.js
@@ -3,23 +3,58 @@ var http = require('http').createServer(app);
 const PORT = 8080;
 var io = require('socket.io')(http, {
     cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
     }
-    });
-const STATIC_CHANNELS =['global_notifications', 'global_chat'];
-// console.log(io);
+});
+var STATIC_CHANNELS = [{
+    name: 'Global chat',
+    participants: 0,
+    id: 1,
+}, {
+    name: 'Funny',
+    participants: 0,
+    id: 2,
+}];
 
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+// console.log(io);
+app.get('/getChannels', (req, res) => {
+    res.json({
+        channels: STATIC_CHANNELS
+    })
+});
 http.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
 });
 io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
 
     console.log('new client connected');
-    socket.emit('connection',null);
+    socket.emit('connection', null);
+    socket.on('channel-join', id => {
+        console.log('channel join', id);
+        STATIC_CHANNELS.forEach(c => {
+            if (c.id === id) {
+                if (c.sockets.indexOf(socket.id) == (-1)) {
+                    c.sockets.push(socket.id);
+                    c.participants++;
+                    io.emit('channel', c);
+                }
+            } else {
+                let index = c.sockets.indexOf(socket.id);
+                if (index != (-1)) {
+                    c.sockets.splice(index, 1);
+                    c.participants--;
+                    io.emit('channel', c);
+                }
+            }
+        });
+ 
+        return id;
+    })
 });
-// app.get('/getChannels', (req, res) => {
-//     res.json({
-//         channels: STATIC_CHANNELS
-//     })
-// });
+
